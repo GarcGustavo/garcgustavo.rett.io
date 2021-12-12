9dc11752e4471fb05b0636226897834350245837
GarcGustavo <gustavo-reyes@live.com>
2021-12-12 15:32:02 -0500

    deploy

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ module.exports = {
     outputDir: 'docs',
     publicPath: process.env.NODE_ENV === 'production'
       ? '/https://github.com/GarcGustavo/garcgustavo.rett.io/'
-      : '/'
+      : './'
   }
